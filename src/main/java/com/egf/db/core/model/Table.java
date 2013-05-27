bda95c9925a781fc35ce9122f7875f7aed2598f7
fangj <fangj@bf59f392-e446-4fc5-b625-b96c7fc0d3f9>
2013-05-27 08:25:38 +0000

    增加了注释

--- src/main/java/com/egf/db/core/model/Table.java
+++ src/main/java/com/egf/db/core/model/Table.java
@@ -6,6 +6,8 @@
  */
 package com.egf.db.core.model;
 
+import java.sql.SQLException;
+
 import com.egf.db.core.define.column.ColumnDefine;
 import com.egf.db.core.define.column.Comment;
 import com.egf.db.core.define.column.Default;
@@ -20,54 +22,184 @@ import com.egf.db.core.define.column.NotNull;
  * @since 1.0
  */
 public interface Table {
-		
+	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param notNull 不为空
+	 */
 	public void varchar2(String name,Limit limit,NotNull notNull);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param notNull 不为空
+	 * @param comment 注释
+	 */
 	public void varchar2(String name,Limit limit,NotNull notNull,Comment comment);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param comment 注释
+	 */
 	public void varchar2(String name,Limit limit,Comment comment);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 */
 	public void varchar2(String name,Limit limit);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param deft	默认值
+	 * @param notNull 不为空
+	 */
 	public void varchar2(String name,Limit limit,Default deft,NotNull notNull);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param deft 默认值
+	 * @param notNull 不为空
+	 * @param comment 注释
+	 */
 	public void varchar2(String name,Limit limit,Default deft,NotNull notNull,Comment comment);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param deft 默认值
+	 * @param comment 注释
+	 */
 	public void varchar2(String name,Limit limit,Default deft,Comment comment);
 	
+	/**
+	 * varchar2
+	 * @param name 名称
+	 * @param limit 长度限制
+	 * @param deft 默认值
+	 */
 	public void varchar2(String name,Limit limit,Default deft);
 	
 	/**
 	 * varchar2类型
 	 * @param name 字段名称
 	 * @param limit 长度
-	 * @param define 其他字段特性定义(NullOrNotNull,Default,Comment)
+	 * @param define 其他字段特性定义
+	 * 可选参数：
+	 * Default 默认值
+	 * Null,NotNull 是否可为空 
+	 * Comment 注释
+	 * @throws SQLException
 	 */
 	public void varchar2(String name,Limit limit, ColumnDefine ... define);
 	
+	/**
+	 * blob
+	 * @param name 名称
+	 * @param notNull 不为空
+	 */
 	public void blob(String name,NotNull notNull);
 	
+	/**
+	 * blob
+	 * @param name 名称
+	 */
 	public void blob(String name);
 	
+	/**
+	 * blob
+	 * @param name 名称
+	 * @param notNull 不为空
+	 * @param comment 注释
+	 */
 	public void blob(String name,NotNull notNull,Comment comment);
 	
+	/**
+	 * blob
+	 * @param name 名称
+	 * @param comment 注释
+	 */
 	public void blob(String name,Comment comment);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param notNull 不为空
+	 */
 	public void number(String name,NotNull notNull);
 	
+	/**
+	 * number 
+	 * @param name 名称
+	 */
 	public void number(String name);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param notNull 不为空
+	 * @param comment 注释
+	 */
 	public void number(String name,NotNull notNull,Comment comment);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param comment 注释
+	 */
 	public void number(String name,Comment comment);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param deft 默认值
+	 * @param notNull 不为空
+	 */
 	public void number(String name,Default deft, NotNull notNull);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param deft 默认值
+	 */
 	public void number(String name,Default deft);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param deft 默认值
+	 * @param notNull 不为空
+	 * @param comment 注释
+	 */
 	public void number(String name,Default deft,NotNull notNull,Comment comment);
 	
+	/**
+	 * number
+	 * @param name 名称
+	 * @param define 其他字段特性定义
+	 * 可选参数：
+	 * Default 默认值
+	 * Null,NotNull 是否可为空 
+	 * Comment 注释
+	 */
 	public void number(String name,ColumnDefine ... define);
 	
+	/**
+	 * number 
+	 * @param name 名称
+	 * @param deft 默认值
+	 * @param comment 注释
+	 */
 	public void number(String name,Default deft,Comment comment);
 }
