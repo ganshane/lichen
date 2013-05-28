0860a459ee7e98142270751b1c5ddf14e1f2437c
fangj <fangj@bf59f392-e446-4fc5-b625-b96c7fc0d3f9>
2013-05-28 05:00:18 +0000

    升级成功，失败记录 最后版本到数据库

--- src/main/java/com/egf/db/command/support/ClientCommand.java
+++ src/main/java/com/egf/db/command/support/ClientCommand.java
@@ -12,7 +12,13 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 import com.egf.db.command.Command;
+import com.egf.db.core.DbConstant;
+import com.egf.db.core.config.SysConfigPropertyUtil;
+import com.egf.db.core.jdbc.JdbcService;
+import com.egf.db.core.jdbc.JdbcServiceImpl;
 import com.egf.db.services.impl.AbstractMigration;
+import com.egf.db.utils.DateTimeUtils;
+import com.egf.db.utils.StringUtils;
 
 /**
  * @author fangj
@@ -25,9 +31,15 @@ public class ClientCommand implements Command {
 
 	public void up(String pack) {
 		AbstractMigration am = null;
+		String timeId=null;
+		String handle=null;
 		Set<Class<?>> classSet = DbScriptFileClassFind.getDbScriptClasses(pack);
 		//安文件时间先后排序
 		for (Class<?> cls : classSet) {
+			String className=cls.getName();
+			String fileName=className.substring(className.lastIndexOf(".")+1,className.length());
+			handle=fileName.split("_")[0];
+			timeId=fileName.split("_")[1];
 			logger.info("\n"+cls.getName() + " up script start run...");
 			try {
 				am = (AbstractMigration) cls.newInstance();
@@ -37,8 +49,12 @@ public class ClientCommand implements Command {
 			try {
 				am.up();
 			} catch (SQLException e) {
+				saveLog(timeId, handle);
 				break;
 			}
+		}
+		if(!StringUtils.isBlank(timeId)&&!StringUtils.isBlank(handle)){
+			saveLog(timeId, handle);
 		}
 	}
 
@@ -46,5 +62,22 @@ public class ClientCommand implements Command {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	/**
+	 * 保存版本信息
+	 * @param timeId id
+	 * @param handle 操作描述
+	 */
+	private void saveLog(String timeId,String handle){
+		JdbcService js=new JdbcServiceImpl();
+		String database_changelog=SysConfigPropertyUtil.getInstance().getPropertyValue(DbConstant.CHANGELOG);
+		String sql =String.format("insert into "+database_changelog+"(id,applied_at,description) values('%s','%s','%s')",timeId,DateTimeUtils.getNowTimeNormalString(),handle);
+		try {
+			logger.info("\n记录版本信息:"+sql);
+			js.execute(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			logger.error("\n记录版本信息出错:"+e.getMessage());
+		}
+	}
 }
