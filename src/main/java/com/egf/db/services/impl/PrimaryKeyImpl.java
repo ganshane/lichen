627a92498ee8179fb36f7438f5f94fe6777b126a
fangj <fangj@bf59f392-e446-4fc5-b625-b96c7fc0d3f9>
2013-05-29 01:56:49 +0000

    修改了isprimarky 为primary

---
+++ src/main/java/com/egf/db/services/impl/PrimaryKeyImpl.java
@@ -0,0 +1,19 @@
+/*		
+ * Copyright 2013-5-29 The EGF Co., Ltd. 
+ * site: http://www.egfit.com
+ * file: $Id: org.eclipse.jdt.ui.prefs,IsPrimaryKeyImpl.java,fangj Exp$
+ * created at:上午10:08:03
+ */
+package com.egf.db.services.impl;
+
+import com.egf.db.core.define.column.PrimaryKey;
+
+/**
+ * 是主键
+ * @author fangj
+ * @version $Revision: 2.0 $
+ * @since 1.0
+ */
+class PrimaryKeyImpl implements PrimaryKey {
+
+}
