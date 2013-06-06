7e16f2be87c7872b3ff5fef10ea6622c4e4b30b8
fangj <fangj@bf59f392-e446-4fc5-b625-b96c7fc0d3f9>
2013-06-06 08:43:38 +0000

    增加了jdbcservice

--- src/main/java/com/egf/db/core/db/AbstractDb.java
+++ src/main/java/com/egf/db/core/db/AbstractDb.java
@@ -6,17 +6,21 @@
  */
 package com.egf.db.core.db;
 
+import com.egf.db.core.jdbc.JdbcService;
+import com.egf.db.core.jdbc.JdbcServiceImpl;
 import com.egf.db.exception.MigrationException;
 
 
-
 /**
+ * 抽象数据库接口
  * @author fangj
  * @version $Revision: 1.0 $
  * @since 1.0
  */
 public abstract class AbstractDb implements DbInterface{
 
+	protected JdbcService jdbcService=new JdbcServiceImpl();
+	
 	public void createSchema(String schema)throws MigrationException{}
 
 }
