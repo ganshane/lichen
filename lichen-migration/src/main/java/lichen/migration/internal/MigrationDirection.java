1c232867dabff775d48bce9b0f324558b318299f
jcai <jcai@ganshane.com>
2013-07-23 15:28:23 +0800

    合并类

--- lichen-migration/src/main/java/lichen/migration/internal/MigrationDirection.java
+++ lichen-migration/src/main/java/lichen/migration/internal/MigrationDirection.java
@@ -1,5 +1,10 @@
 package lichen.migration.internal;
 
+import lichen.migration.services.Migration;
+
+import java.util.SortedMap;
+import java.util.SortedSet;
+
 /**
  * Sealed abstract class that defines the direction to run a
  * migration.
@@ -14,4 +19,31 @@ enum MigratorOperation{
     RollbackMigration;
     int version;
     int count;
+}
+class MigrationStatuses{
+    final SortedMap<Long, Class<? extends Migration>> notInstalled;
+    final SortedMap<Long, Class<? extends Migration>> installedWithAvailableImplementation;
+    final SortedSet<Long> installedWithoutAvailableImplementation;
+
+    /**
+     * Container for the state of all the available and installed
+     * migrations.
+     *
+     * @param notInstalled a sorted map of migration version numbers to
+     *        MigrationHelper subclasses that are not installed in the database
+     * @param installedWithAvailableImplementation a sorted map of
+     *        migration version numbers to MigrationHelper subclasses that are
+     *        currently installed in the database that have a matching a
+     *        MigrationHelper subclass
+     * @param installedWithoutAvailableImplementation a sorted set of
+     *        migration version numbers that are currently installed in
+     *        the database but do not have a matching a MigrationHelper subclass
+     */
+    MigrationStatuses(SortedMap<Long, Class<? extends Migration>> notInstalled,
+                      SortedMap<Long, Class<? extends Migration>> installedWithAvailableImplementation,
+                      SortedSet<Long> installedWithoutAvailableImplementation){
+        this.notInstalled = notInstalled;
+        this.installedWithAvailableImplementation = installedWithAvailableImplementation;
+        this.installedWithoutAvailableImplementation = installedWithoutAvailableImplementation;
+    }
 }
