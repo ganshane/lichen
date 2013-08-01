66df75f21ef81e7160f6011d3eb12ea4e9ffce3b
jcai <jcai@ganshane.com>
2013-08-01 21:15:11 +0800

    增加两个接口，用于设置值和获取值

---
+++ lichen-activerecord/src/main/java/lichen/jdbc/services/PreparedStatementSetter.java
@@ -0,0 +1,18 @@
+package lichen.jdbc.services;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * 针对{@link java.sql.PreparedStatement}设置值
+ * @author jcai
+ */
+public interface PreparedStatementSetter {
+    /**
+     * 对PreparedStatement设置值
+     * @param ps {@link PreparedStatement}对象
+     * @param index 设置的索引号
+     * @throws SQLException 发生数据库操作错误
+     */
+    public void set(PreparedStatement ps,int index) throws SQLException;
+}
