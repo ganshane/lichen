dab6afde63c901b78b41b313f88f9f737aa9230a
zhaoyong <zhaoyong@dev.egfit.com>
2013-08-06 16:08:43 +0800

    重新修改StringUtils类的实现，并添加测试用例
    Change-Id: I0148f75d6092c9761126580a37f2a3ebf90cf29b

---
+++ lichen-migration/src/test/java/lichen/migration/testutil/StringUtilsTest.java
@@ -0,0 +1,26 @@
+package lichen.migration.testutil;
+
+import junit.framework.Assert;
+import lichen.migration.util.StringUtils;
+
+import org.junit.Test;
+
+/**
+ * StringUtils的单元测试类
+ * @author zhaoyong
+ *
+ */
+public class StringUtilsTest {
+	
+	/**
+	 * 方法：join(String[] array, String sperator)的单元测试用例
+	 */
+	@Test
+	public void test_join() {
+		String[] array = new String[]{"a", "b", "c"};
+		Assert.assertEquals("a_b_c", StringUtils.join(array, "_"));	//将数组用下划线隔开
+		Assert.assertEquals("a,b,c", StringUtils.join(array, ","));	//将数组用逗号隔开
+		Assert.assertEquals("abc", StringUtils.join(array, null));	//当分隔符为空时，默认分隔符为空串
+		Assert.assertEquals("", StringUtils.join(null, null));	//当数组为空时，直接返回空串
+	}
+}
