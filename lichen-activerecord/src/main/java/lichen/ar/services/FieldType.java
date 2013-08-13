0f46b649b6fa50f2169b3f2cde1f4a0157b57a01
jcai <jcai@ganshane.com>
2013-08-13 10:16:12 +0800

    FIXES #34 修正FieldType的checkstyle风格

--- lichen-activerecord/src/main/java/lichen/ar/services/FieldType.java
+++ lichen-activerecord/src/main/java/lichen/ar/services/FieldType.java
@@ -5,25 +5,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * 定义某一字段的类型,用来从数据库中取得对象以及从设置数据库对象
+ * 定义某一字段的类型,用来从数据库中取得对象以及从设置数据库对象.
+ *
  * @author jcai
  */
-public interface FieldType<T>{
+public interface FieldType<T> {
     /**
-     * 设置字段的对象
-     * @param ps PreparedStatement对象
-     * @param index 索引序列
+     * 设置字段的对象.
+     *
+     * @param ps     PreparedStatement对象
+     * @param index  索引序列
      * @param object 对应的值
      * @throws SQLException 发生数据库异常
      */
-    public void set(PreparedStatement ps,int index,T object) throws SQLException;
+    abstract void set(PreparedStatement ps, int index, T object) throws SQLException;
 
     /**
-     * 从数据库中得到某一个值
-     * @param rs 结果集对象
+     * 从数据库中得到某一个值.
+     *
+     * @param rs    结果集对象
      * @param index 结果集中索引
      * @return 对象值
      * @throws SQLException 发生数据库异常
      */
-    public T get(ResultSet rs,int index) throws SQLException;
+    abstract T get(ResultSet rs, int index) throws SQLException;
 }
