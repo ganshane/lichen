e094fc5f146b42967653243db085a5e044384c46
Jun Tsai <jcai@ganshane.com>
2014-06-05 13:29:17 +0800

    增加事物对于方法上的AOP操作

---
+++ creeper/src/main/java/creeper/core/internal/TransactionAdvice.java
@@ -0,0 +1,59 @@
+package creeper.core.internal;
+
+import creeper.core.services.CreeperException;
+import org.apache.tapestry5.ioc.annotations.Local;
+import org.apache.tapestry5.plastic.MethodAdvice;
+import org.apache.tapestry5.plastic.MethodInvocation;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Method;
+
+/**
+ * 针对事物的AOP处理
+ * @author jcai
+ */
+public class TransactionAdvice implements MethodAdvice{
+    private TransactionInterceptor interceptor;
+
+    public TransactionAdvice(@Local TransactionInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void advise(final MethodInvocation methodInvocation) {
+        try {
+            this.interceptor.invoke(new org.aopalliance.intercept.MethodInvocation() {
+
+                @Override
+                public Method getMethod() {
+                    return methodInvocation.getMethod();
+                }
+
+                @Override
+                public Object[] getArguments() {
+                    throw new UnsupportedOperationException();
+
+                }
+
+                @Override
+                public Object proceed() throws Throwable {
+                    methodInvocation.proceed();
+                    return methodInvocation.getReturnValue();
+                }
+
+                @Override
+                public Object getThis() {
+                    return null;
+                }
+
+                @Override
+                public AccessibleObject getStaticPart() {
+                    throw new UnsupportedOperationException();
+                }
+            });
+        } catch (Throwable throwable) {
+            CreeperException.wrap(throwable);
+        }
+    }
+}
--- creeper/src/main/java/creeper/core/services/CreeperJpaModule.java
+++ creeper/src/main/java/creeper/core/services/CreeperJpaModule.java
@@ -1,21 +1,31 @@
 package creeper.core.services;
 
 import creeper.core.config.CreeperCoreConfig;
+import creeper.core.internal.TransactionAdvice;
 import org.apache.tapestry5.ioc.*;
 import org.apache.tapestry5.ioc.annotations.Contribute;
 import org.apache.tapestry5.ioc.annotations.Local;
+import org.apache.tapestry5.ioc.annotations.Match;
 import org.apache.tapestry5.ioc.services.MasterObjectProvider;
+import org.apache.tapestry5.plastic.*;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.support.StaticListableBeanFactory;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
+import org.springframework.data.repository.util.TxUtils;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.SharedEntityManagerCreator;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -28,6 +38,7 @@ import java.util.Properties;
 public class CreeperJpaModule {
     public static void bind(ServiceBinder binder){
         binder.bind(JpaVendorAdapter.class, HibernateJpaVendorAdapter.class);
+        binder.bind(TransactionAdvice.class);
     }
     //创建基于Hibernate的JPA实现
     public static EntityManagerFactory buildEntityManagerFactory(CreeperCoreConfig config,
@@ -73,23 +84,17 @@ public class CreeperJpaModule {
                                               @Local
                                                final DaoPackageManager entityPackageManager,
                                               @Local
-                                               final JpaTransactionManager transactionManager,
+                                               final BeanFactory beanFactory,
                                               @Local
-                                               final EntityManagerFactory entityManagerFactory,
-                                              @Local
-                                               final JpaVendorAdapter jpaVendorAdapter)
+                                               final EntityManagerFactory entityManagerFactory)
     {
-        final StaticListableBeanFactory beanFactory = new StaticListableBeanFactory();
-        beanFactory.addBean("transactionManager",transactionManager);
-        beanFactory.addBean("entityManagerFactory",entityManagerFactory);
-        beanFactory.addBean("jpaDialect", jpaVendorAdapter.getJpaDialect());
         configuration.add("entityDaoProvider", new ObjectProvider() {
             @Override
             public <T> T provide(Class<T> objectType, AnnotationProvider annotationProvider, ObjectLocator locator) {
                 if (entityPackageManager.contains(objectType) && objectType.isInterface()) {
                     JpaRepositoryFactoryBean jpaRepositoryFactoryBean = new JpaRepositoryFactoryBean();
                     jpaRepositoryFactoryBean.setBeanFactory(beanFactory);
-                    jpaRepositoryFactoryBean.setTransactionManager("transactionManager");
+                    jpaRepositoryFactoryBean.setTransactionManager(TxUtils.DEFAULT_TRANSACTION_MANAGER);
                     EntityManager entityManager = SharedEntityManagerCreator.createSharedEntityManager(entityManagerFactory);
                     jpaRepositoryFactoryBean.setEntityManager(entityManager);
                     jpaRepositoryFactoryBean.setRepositoryInterface(objectType);
@@ -100,4 +105,35 @@ public class CreeperJpaModule {
             }
         });
     }
+    public static ListableBeanFactory buildBeanFactory(@Local
+            final JpaTransactionManager transactionManager,
+            @Local
+            final EntityManagerFactory entityManagerFactory,
+            @Local
+            final JpaVendorAdapter jpaVendorAdapter){
+        final StaticListableBeanFactory beanFactory = new StaticListableBeanFactory();
+        beanFactory.addBean(TxUtils.DEFAULT_TRANSACTION_MANAGER,transactionManager);
+        beanFactory.addBean("entityManagerFactory",entityManagerFactory);
+        beanFactory.addBean("jpaDialect", jpaVendorAdapter.getJpaDialect());
+        return beanFactory;
+    }
+    public static TransactionInterceptor buildTransactionInterceptor(@Local BeanFactory beanFactory){
+        AnnotationTransactionAttributeSource transactionAttributeSource = new AnnotationTransactionAttributeSource();
+
+        TransactionInterceptor transactionInterceptor = new TransactionInterceptor(null, transactionAttributeSource);
+        transactionInterceptor.setTransactionManagerBeanName(TxUtils.DEFAULT_TRANSACTION_MANAGER);
+        transactionInterceptor.setBeanFactory(beanFactory);
+        transactionInterceptor.afterPropertiesSet();
+        return transactionInterceptor;
+    }
+    @Match("*")
+    public static void adviseTransactional(MethodAdviceReceiver receiver,@Local TransactionAdvice transactionAdvice)
+    {
+        for (Method m : receiver.getInterface().getMethods())
+        {
+            //仅仅对含有Transactional注解的方法进行AOP
+            if (receiver.getMethodAnnotation(m, Transactional.class) != null)
+                receiver.adviseMethod(m, transactionAdvice);
+        }
+    };
 }
