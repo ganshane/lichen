eca267d7e4765c5628978c8d4a0e9f8ecd9e731b
Jun Tsai <jcai@ganshane.com>
2014-06-10 13:15:34 +0800

    删除没用的代码

--- lichen-migration/src/main/java/lichen/migration/model/SequenceOption.java
+++ lichen-migration/src/main/java/lichen/migration/model/SequenceOption.java
@@ -6,55 +6,5 @@ package lichen.migration.model;
  *
  */
 public interface SequenceOption {
-	
 	int getValue();
-	
-//	/**
-//	 * 最小值
-//	 * @param minvalue
-//	 */
-//	void minValue(int minvalue);
-//	
-//	/**
-//	 * 最大值
-//	 * @param maxvalue
-//	 */
-//	void maxValue(int maxvalue);
-//	
-//	/**
-//	 * 起始值
-//	 * @param start
-//	 */
-//	void start(int start);
-//	
-//	/**
-//	 * 增量
-//	 * @param increment
-//	 */
-//	void increment(int increment);
-//	
-//	/**
-//	 * 获取最小值
-//	 * @return
-//	 */
-//	int getMinValue();
-//	
-//	/**
-//	 * 获取最大值
-//	 * @return
-//	 */
-//	int getMaxValue();
-//	
-//	/**
-//	 * 获取起始值
-//	 * @return
-//	 */
-//	int getStart();
-//	
-//	/**
-//	 * 获取增量
-//	 * @return
-//	 */
-//	int getIncrement();
-	
 }
