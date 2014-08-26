ac92df644eb3170900774447ce57d8395cbed877
shenlg <43543865@qq.com>
2014-08-26 16:48:24 +0800

    修改字段名

--- lichen-creeper/src/main/java/lichen/creeper/user/db/Migrate_20140606010101_CreateUsers.java
+++ lichen-creeper/src/main/java/lichen/creeper/user/db/Migrate_20140606010101_CreateUsers.java
@@ -43,7 +43,7 @@ public class Migrate_20140606010101_CreateUsers implements Migration {
         _helper.commentColumn("users", "mail", "电子邮件");
         _helper.commentColumn("users", "data", "存放人员数据");
         _helper.commentColumn("users", "created", "创建时间，为秒数");
-        _helper.commentColumn("users", "access", "最后一次访问时间，为秒数");
+        _helper.commentColumn("users", "last_access", "最后一次访问时间，为秒数");
         _helper.commentColumn("users", "login", "最后一次成功登录的时间，为秒数");
         _helper.commentColumn("users", "status", "状态,0为正常，1为拒绝");
         _helper.addIndex("users","name");
