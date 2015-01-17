5b954355ab5d2a063c472e1f27dd27bb04a7d4f8
Jun Tsai <jcai@ganshane.com>
2015-01-17 13:08:48 +0800

    #1 还原原始代码

--- lichen-core/src/test/java/lichen/core/services/WordUtilTest.java
+++ lichen-core/src/test/java/lichen/core/services/WordUtilTest.java
@@ -36,7 +36,7 @@ public class WordUtilTest {
     
     @Test
     public void test_map_plural2singles() {
-    	for (Map.Entry<String, String> entry : WordUtil.PLURAL_2SINGLES.entrySet()) {
+    	for (Map.Entry<String, String> entry : WordUtil.plural2singles.entrySet()) {
     		String pword = entry.getKey();
     		String sword = entry.getValue();
     		String result = WordUtil.pluralize(sword);
